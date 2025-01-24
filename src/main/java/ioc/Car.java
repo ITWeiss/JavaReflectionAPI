a440de772d53fcb7dd27408ce2563fc2ce83b8f8
ITWeiss <gottfried98@bk.ru>
2025-01-24 21:38:40 +0700

    IOC-Container

--- src/main/java/ioc/Car.java
+++ src/main/java/ioc/Car.java
@@ -1,4 +1,14 @@
 package ioc;
 
+@Component
+@Scope()
 public class Car {
+
+    @Inject
+    private Engine engine;
+
+    void drive() {
+        engine.start();
+        System.out.println("Car is driving.");
+    }
 }
